edbf226cdff6aafcb522831536c2f312702f2a84
tahoa40007-sys <tahoa40007@gmail.com>
2025-12-19 14:09:01 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -11,9 +11,8 @@ const APP_SHELL = [
   "/index.html",
   "/styles.css",
   "/app.js",
-  "/manifest.webmanifest",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/manifest.webmanifest"
+
 ];
 
 self.addEventListener("install", (event) => {
